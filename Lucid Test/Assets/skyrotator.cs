d4126144b9abde1887decb291a959bca84bddc4d
cwiggsi <carl_wiggins@bloomfield.edu>
2019-02-13 18:13:11 -0500

    forest scene

---
+++ Lucid Test/Assets/skyrotator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class skyrotator : MonoBehaviour
+{
+
+    public float RotateSpeed = 1.2f;
+    void update()
+    {
+        RenderSettings.skybox.SetFloat("_Rotation", Time.time * RotateSpeed);
+    }
+}
